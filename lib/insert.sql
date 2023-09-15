697b8c8a626fb564bd52a1d3427e73d2e17f4407
Rae Stanton <raestanton93@gmail.com>
2023-09-15 10:08:18 -0700

    fin

---
+++ lib/insert.sql
@@ -0,0 +1,142 @@
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  1,
+  "Mr. Chocolate",
+  3,
+  "M",
+  "brown",
+  "chill",
+  "True"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  2,
+  "Rowdy",
+  2,
+  "F",
+  "brown",
+  "chill",
+  "True"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  3,
+  "Tabitha",
+  3,
+  "M",
+  "white",
+  "chill",
+  "True"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  4,
+  "Sergeant Brown",
+  3,
+  "M",
+  "brown",
+  "angy",
+  "True"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  5,
+  "Melissa",
+  8,
+  "F",
+  "rainbow",
+  "prideful",
+  "True"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  6,
+  "Grinch",
+  85,
+  "M",
+  "green",
+  "angy",
+  "False"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  7,
+  "Wendy",
+  250,
+  "M",
+  "brown",
+  "wistful",
+  "False"
+);
+INSERT INTO bears (
+  id,
+  name,
+  age,
+  sex,
+  color,
+  temperament,
+  alive
+)
+VALUES (8,
+null,
+20,
+"M",
+"black",
+"aggressive",
+0);
